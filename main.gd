56b4d19ecaeb631cfaa1a9d686aba62bc14cb549
Ramiro Sánchez <ramiro.sanchez@kaleidos.net>
2016-07-14 11:06:10 +0200

    Add new player configs

--- main.gd
+++ main.gd
@@ -2,51 +2,52 @@
 extends Node2D
 
 const player1_config = {
-	"player_speed": 220,
-	"dash_duration": 200,
-	"dash_speed": 520,
-	"time_movement_block_after_shot": 300,
+	"player_speed": 160,
+	"dash_duration": 190,
+	"dash_speed": 350,
+	"time_movement_block_after_shot": 200,
 	"time_automatic_shot": 2000,
 	"shot_angles": {
-		"up": Vector2(1.5, -4),
-		"up-middle": Vector2(1.5, -2),
-		"down-middle": Vector2(1.5, 1),
-		"down": Vector2(1.5, 4)
+		"up": Vector2(1.5, -3),
+		"up-middle": Vector2(1.5, -1.5),
+		"down-middle": Vector2(1.5, 1.5),
+		"down": Vector2(1.5, 3)
 	},
 	"shots_strength": {
 		"normal_shots": [
 			{"min": 0, "max": 100, "speed": 700},
-			{"min": 100, "max": 300, "speed": 500},
-			{"min": 300, "speed": 200}
+			{"min": 100, "max": 300, "speed": 600},
+			{"min": 300, "speed": 500}
 		],
 		"normal_angle_shots": [
-			{"min": 0, "speed": 700, "angles": ["up", "down"]},
-			{"min": 0, "speed": 600, "angles": ["up-middle", "down-middle"]}
+			{"min": 0, "speed": 600, "angles": ["up", "down"]},
+			{"min": 0, "speed": 500, "angles": ["up-middle", "down-middle"]}
 		]
 	}
 }
 
+
 const player2_config = {
-	"player_speed": 220,
-	"dash_duration": 200,
-	"dash_speed": 520,
-	"time_movement_block_after_shot": 300,
+	"player_speed": 160,
+	"dash_duration": 190,
+	"dash_speed": 350,
+	"time_movement_block_after_shot": 200,
 	"time_automatic_shot": 2000,
 	"shot_angles": {
-		"up": Vector2(1.5, -4),
-		"up-middle": Vector2(1.5, -2),
-		"down-middle": Vector2(1.5, 1),
-		"down": Vector2(1.5, 4)
+		"up": Vector2(1.5, -3),
+		"up-middle": Vector2(1.5, -1.5),
+		"down-middle": Vector2(1.5, 1.5),
+		"down": Vector2(1.5, 3)
 	},
 	"shots_strength": {
 		"normal_shots": [
 			{"min": 0, "max": 100, "speed": 700},
-			{"min": 100, "max": 300, "speed": 500},
-			{"min": 300, "speed": 200}
+			{"min": 100, "max": 300, "speed": 600},
+			{"min": 300, "speed": 500}
 		],
 		"normal_angle_shots": [
-			{"min": 0, "speed": 700, "angles": ["up", "down"]},
-			{"min": 0, "speed": 600, "angles": ["up-middle", "down-middle"]}
+			{"min": 0, "speed": 600, "angles": ["up", "down"]},
+			{"min": 0, "speed": 500, "angles": ["up-middle", "down-middle"]}
 		]
 	}
 }
